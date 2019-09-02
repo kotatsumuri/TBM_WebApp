8ace2020c4073eda0fa0c8e7e4543fc030491103
kotatsumuri <38883126+kotatsumuri@users.noreply.github.com>
2019-09-02 20:28:15 +0900

    少し改善

--- tbm-web-app/src/App.vue
+++ tbm-web-app/src/App.vue
@@ -1,14 +1,14 @@
 <template>
     <div id = "app">
-      <v-app v-if = "$store.getters.rdbLoaded" >
-        <page-header/>
-        <v-content>
-          <v-container fluid>
-            <router-view/>
-          </v-container>
-        </v-content>
-        <page-footer/>
-      </v-app>
+        <v-app>
+            <page-header/>
+            <v-content v-if = "$store.getters.rdbLoaded">
+                <v-container fluid>
+                    <router-view/>
+                </v-container>
+            </v-content>
+            <page-footer/>
+        </v-app>
     </div>
 </template>
 
@@ -18,34 +18,34 @@ import PageFooter from './components/PageFooter'
 import firebase from 'firebase';
 
 export default {
-  name: 'App',
+    name: 'App',
 
-  components: {
-    PageHeader,
-    PageFooter,
-  },
+    components: {
+        PageHeader,
+        PageFooter,
+    },
 
-  data () {
-    return {
+    data () {
+        return {
+        }
+    },
+
+    created: function() {
+        try {
+            firebase.database().ref('datas/').on('value',snapshot => {
+                this.$store.commit('setTrashBoxDatas',snapshot.val());
+            });
+            setTimeout(this.$store.commit, 1000,'finishLoad');
+        }
+        catch(e) {
+            /* eslint-disable */
+            console.error(e)
+        }
+    },
+
+    mounted: function() {
+        
     }
-  },
-
-  created: function() {
-    try {
-      firebase.database().ref('datas/').on('value',snapshot => {
-        this.$store.commit('setTrashBoxDatas',snapshot.val());
-      });
-      this.$store.commit('finishLoad');
-    }
-    catch(e) {
-      /* eslint-disable */
-      console.error(e)
-    }
-  },
-
-  mounted: function() {
-    
-  }
 
 }
 </script>
--- tbm-web-app/src/components/TrashBoxGraph.vue
+++ tbm-web-app/src/components/TrashBoxGraph.vue
@@ -46,6 +46,9 @@ export default {
                     toolbar: {
                         show: false,
                     },
+                    animations: {
+                        enabled: false
+                    }
                 },
                 xaxis:{
                     type: 'datetime',
